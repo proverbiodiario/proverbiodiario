8d97a254f7cc8c08e698f5ff9331a3a2ee436a4a
proverbiodiario <159117126+proverbiodiario@users.noreply.github.com>
2024-02-06 19:03:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-      // Função para selecionar um provérbio aleatório
-                              function selecionarProverbioAleatorio() {
-                                        // Lista de provérbios
-          var provérbios = [
-                        "Provérbio 1",
-                        "Provérbio 2",
-                        "Provérbio 3",
-                        "Provérbio 4",
-                        "Provérbio 5",
-                        "Provérbio 6",
-                        "Provérbio 7",
-                        "Provérbio 8",
-                        "Provérbio 9",
-                        "Provérbio 10",
-                        "Provérbio 11",
-                        "Provérbio 12",
-                        "Provérbio 13",
-                        "Provérbio 14",
-                        "Provérbio 15",
-                        "Provérbio 16",
-                        "Provérbio 17",
-                        "Provérbio 18",
-                        "Provérbio 19"
-                    ];
-
-          // Selecionar um provérbio aleatório
+// Função para selecionar um provérbio aleatório
+function selecionarProverbioAleatorio() {
           var indice = Math.floor(Math.random() * provérbios.length);
-                                        return provérbios[indice];
-                              }
+          return provérbios[indice];
+}
 
-                              // Função para atualizar o provérbio do dia
-                              function atualizarProverbio() {
-                                        var proverbioElement = document.getElementById("proverbio");
-                                        var proverbio = selecionarProverbioAleatorio();
+// Função para atualizar o provérbio do dia
+function atualizarProverbioDoDia() {
+          var dataAtual = new Date();
+          var horaAtual = dataAtual.getUTCHours(); // Horário UTC
+    var novoProverbio = selecionarProverbioAleatorio();
 
-          // Exibir o provérbio na página
-          proverbioElement.textContent = proverbio;
-                              }
+    // Se for meia-noite ou meio-dia, atualize o provérbio
+    if (horaAtual === 0 || horaAtual === 12) {
+                  var proverbioElement = document.getElementById("proverbio");
+                  proverbioElement.innerHTML = "<p>" + novoProverbio + "</p>";
+    }
+}
 
-                              // Chamar a função para atualizar o provérbio ao carregar a página
-                              atualizarProverbio();
-});
+// Atualizar o provérbio do dia inicialmente
+atualizarProverbioDoDia();
+
+// Atualizar o provérbio a cada hora para verificar se é meia-noite ou meio-dia
+setInterval(atualizarProverbioDoDia, 3600000); // 3600000 milissegundos = 1 hora
