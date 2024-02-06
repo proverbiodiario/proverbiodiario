398ead15f7114b92e35eff5b90c80c754bfd9a40
proverbiodiario <159117126+proverbiodiario@users.noreply.github.com>
2024-02-06 19:35:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 // Função para selecionar um provérbio aleatório
 function selecionarProverbioAleatorio() {
-          var indice = Math.floor(Math.random() * provérbios.length);
-          return provérbios[indice];
+    var indice = Math.floor(Math.random() * provérbios.length);
+    return provérbios[indice];
 }
 
 // Função para atualizar o provérbio do dia
 function atualizarProverbioDoDia() {
-          var dataAtual = new Date();
-          var horaAtual = dataAtual.getUTCHours(); // Horário UTC
-    var novoProverbio = selecionarProverbioAleatorio();
+    var dataAtual = new Date();
+    var horaAtual = dataAtual.getUTCHours(); // Obtém a hora em UTC
+    var horaBrasilia = (horaAtual - 3 + 24) % 24; // Ajusta para o horário de Brasília (UTC-3)
 
-    // Se for meia-noite ou meio-dia, atualize o provérbio
-    if (horaAtual === 0 || horaAtual === 12) {
-                  var proverbioElement = document.getElementById("proverbio");
-                  proverbioElement.innerHTML = "<p>" + novoProverbio + "</p>";
+    // Se for meia-noite em Brasília, atualize o provérbio
+    if (horaBrasilia === 0) {
+        var novoProverbio = selecionarProverbioAleatorio();
+        var proverbioElement = document.getElementById("proverbio");
+        proverbioElement.innerHTML = "<h2 class='proverbio-titulo'>Provérbio do Dia</h2><p>" + novoProverbio + "</p>";
     }
 }
 
 // Atualizar o provérbio do dia inicialmente
 atualizarProverbioDoDia();
 
-// Atualizar o provérbio a cada hora para verificar se é meia-noite ou meio-dia
-setInterval(atualizarProverbioDoDia, 3600000); // 3600000 milissegundos = 1 hora
+// Calcular o tempo restante até a próxima meia-noite em Brasília e atualizar o provérbio nesse momento
+var dataAtual = new Date();
+var horaAtual = dataAtual.getUTCHours(); // Obtém a hora em UTC
+var minutosRestantes = (24 - horaAtual - 3) * 60; // Calcula os minutos restantes até a próxima meia-noite em Brasília
+var milissegundosRestantes = minutosRestantes * 60000; // Converte minutos em milissegundos
+setTimeout(function() {
+    setInterval(atualizarProverbioDoDia, 86400000); // 86400000 milissegundos = 24 horas
+}, milissegundosRestantes);
