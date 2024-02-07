242ef48f9fa959fa6e0b6b946e24cbecdbb320ef
proverbiodiario <159117126+proverbiodiario@users.noreply.github.com>
2024-02-07 20:31:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-// Função para selecionar um provérbio aleatório e exibi-lo na página
-function exibirProverbio() {
-                var proverbioElement = document.getElementById("proverbio");
-                var novoProverbio = selecionarProverbioAleatorio();
-                proverbioElement.textContent = novoProverbio;
+// Array de provérbios
+var proverbios = [
+      "Provérbio 1",
+      "Provérbio 2",
+      "Provérbio 3",
+      // Adicione mais provérbios conforme necessário
+  ];
+
+// Seleciona um provérbio aleatório
+function selecionarProverbioAleatorio() {
+      var indice = Math.floor(Math.random() * proverbios.length);
+      return proverbios[indice];
+}
+
+// Função para atualizar o provérbio na página
+function atualizarProverbio() {
+      var proverbioElement = document.getElementById("proverbio");
+      var novoProverbio = selecionarProverbioAleatorio();
+      proverbioElement.textContent = novoProverbio;
 }
 
 // Chamada inicial para exibir um provérbio quando a página é carregada
-exibirProverbio();
+atualizarProverbio();
