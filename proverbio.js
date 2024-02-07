d9d0469690b55e5fc7b2bd6a37300f771a93c179
proverbiodiario <159117126+proverbiodiario@users.noreply.github.com>
2024-02-07 19:28:15 -0300

    Add files via upload

--- proverbio.js
+++ proverbio.js
@@ -1,10 +1,10 @@
 // Array de provérbios
 var provérbios = [
-      "Provérbio 1",
-      "Provérbio 2",
-      "Provérbio 3",
-      // Adicione mais provérbios conforme necessário
-  ];
+    "Provérbio 1",
+    "Provérbio 2",
+    "Provérbio 3",
+    // Adicione mais provérbios conforme necessário
+];
 
-// Exporta a lista de provérbios para uso externo
+// Exporte o array para uso externo
 export { provérbios };
