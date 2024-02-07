393682718d6386de085a1db388e73e2be25c5e0e
proverbiodiario <159117126+proverbiodiario@users.noreply.github.com>
2024-02-07 20:12:49 -0300

    Update proverbios.js

--- proverbios.js
+++ proverbios.js
@@ -1,7 +1,13 @@
 // Array de provérbios
-var provérbios = [
-          "Provérbio 1",
-          "Provérbio 2",
-          "Provérbio 3",
-          // Adicione mais provérbios conforme necessário
-      ];
+var proverbios = [
+              "Provérbio 1",
+              "Provérbio 2",
+              "Provérbio 3",
+              // Adicione mais provérbios conforme necessário
+          ];
+
+// Seleciona um provérbio aleatório
+function selecionarProverbioAleatorio() {
+              var indice = Math.floor(Math.random() * proverbios.length);
+              return proverbios[indice];
+}
