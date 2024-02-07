50762090a2c441f11ee4836341092e7b11d10b39
proverbiodiario <159117126+proverbiodiario@users.noreply.github.com>
2024-02-07 19:10:05 -0300

    Update proverbio.js

--- proverbio.js
+++ proverbio.js
@@ -1,1 +1,10 @@
+// Array de provérbios
+var provérbios = [
+      "Provérbio 1",
+      "Provérbio 2",
+      "Provérbio 3",
+      // Adicione mais provérbios conforme necessário
+  ];
 
+// Exporta a lista de provérbios para uso externo
+export { provérbios };
