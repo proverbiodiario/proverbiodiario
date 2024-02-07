3dfdc3c1cdc439d33cb7e50d3832280263641cdf
proverbiodiario <159117126+proverbiodiario@users.noreply.github.com>
2024-02-07 16:15:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-// Array de provérbios
-var provérbios = [
-    "Provérbio 1",
-    "Provérbio 2",
-    "Provérbio 3",
-    // Adicione mais provérbios conforme necessário
-];
+import { provérbios } from './proverbio.js'; // Importa o array de provérbios
 
-// Seleciona um provérbio aleatório
-var provérbioDoDia = provérbios[Math.floor(Math.random() * provérbios.length)];
+// Função para selecionar um provérbio aleatório
+function selecionarProverbioAleatorio() {
+    var indice = Math.floor(Math.random() * provérbios.length);
+    return provérbios[indice];
+}
 
-// Função para atualizar o provérbio na página HTML
-function atualizarProverbioNaPagina(proverbio) {
-    var proverbioElement = document.getElementById("proverbio");
-    proverbioElement.innerHTML = "<h2 class='proverbio-titulo'>Provérbio do Dia</h2>" +
-                                  "<p class='proverbio-texto'>" + provérbio + "</p>" +
-                                  "<p class='proverbio-fonte'>Fonte: Bíblia Sagrada</p>";
+// Função para atualizar o provérbio do dia
+function atualizarProverbioDoDia() {
+    var dataAtual = new Date();
+    var horaAtual = dataAtual.getHours();
+
+    // Se for meia-noite, atualize o provérbio
+    if (horaAtual === 0) {
+        var provérbioElement = document.getElementById("proverbio");
+        var novoProverbio = selecionarProverbioAleatorio();
+
+        provérbioElement.innerHTML = "<h2 class='proverbio-titulo'>Provérbio do Dia</h2>" +
+                                     "<p class='proverbio-texto'>" + novoProverbio + "</p>" +
+                                     "<p class='proverbio-fonte'>Fonte: Bíblia Sagrada</p>";
+    }
 }
 
 // Chamada inicial para exibir o provérbio do dia
-atualizarProverbioNaPagina(provérbioDoDia);
+atualizarProverbioDoDia();
+
+// Atualizar o provérbio a cada hora para verificar se é meia-noite
+setInterval(atualizarProverbioDoDia, 3600000); // 3600000 milissegundos = 1 hora
