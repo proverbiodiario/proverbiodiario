7f038c29bd1e9a92ffc4141be630d50ceb0a7451
proverbiodiario <159117126+proverbiodiario@users.noreply.github.com>
2024-02-07 16:14:42 -0300

    Update proverbio.js

--- proverbio.js
+++ proverbio.js
@@ -6,11 +6,5 @@ var provérbios = [
     // Adicione mais provérbios conforme necessário
 ];
 
-// Seleciona um provérbio aleatório
-function selecionarProverbioAleatorio() {
-    var indice = Math.floor(Math.random() * provérbios.length);
-    return provérbios[indice];
-}
-
-// Exporta a função para uso externo
-export { selecionarProverbioAleatorio };
+// Exporte o array para uso externo
+export { provérbios };
