87dadfcaf763cedc8ab980efef3d548c564520d1
proverbiodiario <159117126+proverbiodiario@users.noreply.github.com>
2024-02-08 05:56:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,25 @@ provérbioElement.innerText = provérbioArmazenado;
 
 <script>
     // Substitua isso pela sua lista de provérbios
-    var proverbios = [   ];
+    var proverbios = ["Provérbios de Salomão, filho de Davi, rei de Israel:",
+"Para se conhecer a sabedoria e a instrução; para se entenderem as palavras de entendimento;",
+"Para se receber a instrução do entendimento, a justiça, o juízo, e a equidade;",
+"Para dar aos simples, prudência, e aos moços, conhecimento e bom siso;",
+"O sábio ouvirá e crescerá em conhecimento, e o entendido adquirirá sábios conselhos;",
+"Para entender os provérbios e sua interpretação; as palavras dos sábios e suas proposições.",
+"O temor do Senhor é o princípio do conhecimento, mas os loucos desprezam a sabedoria e a instrução.",
+"Filho meu, ouve a instrução de teu pai, e não deixes o ensinamento de tua mãe;",
+"Porque serão como diadema gracioso em tua cabeça, e colares ao teu pescoço.",
+"Filho meu, se os pecadores procurarem te atrair, não consintas.",
+"Se disserem: Vem conosco; embosquemo-nos para derramar sangue, espreitemos sem motivo o inocente;",
+"Traguemo-los vivos, como a sepultura, e inteiros, como os que descem à cova;",
+"Acharemos toda a sorte de bens preciosos, encheremos as nossas casas de despojos;",
+"Lança a tua sorte conosco; teremos todos uma só bolsa!",
+"Filho meu, não te ponhas a caminho com eles; desvia o teu pé das suas veredas.",
+"Porque os pés deles correm para o mal, e se apressam a derramar sangue.",
+"Sem razão se estende a rede diante de todo o olho que há nas asas;",
+"Ora, estes se põem à espreita de sangue, armam ciladas contra a sua própria vida.",
+"Assim são as veredas de todo aquele que usa de cobiça; ela põe a perder a alma dos que a possuem." ];
 
     window.onload = function() {
         var dataAtual = new Date();
