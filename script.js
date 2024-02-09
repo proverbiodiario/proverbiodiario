b58a3b52aea96df52ad44deaf214a0482710760d
proverbiodiario <159117126+proverbiodiario@users.noreply.github.com>
2024-02-09 06:15:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ import { proverbios } from './proverbios.js';
 // Função para atualizar o provérbio
 function atualizarProverbio() {
     var dataAtual = new Date();
-    var diaDoAno = Math.floor(dataAtual.getTime() / (1000 * 60 * 60 * 24));
+    // Ajuste para o fuso horário de Brasília
+    var offsetBrasilia = -3; // Brasília é GMT-3
+    var diaDoAno = Math.floor((dataAtual.getTime() + offsetBrasilia * 60 * 60 * 1000) / (1000 * 60 * 60 * 24));
     var proverbioDoDia = proverbios[diaDoAno % proverbios.length];
     localStorage.setItem('proverbio', proverbioDoDia);
 
@@ -12,16 +14,24 @@ function atualizarProverbio() {
     proverbioElement.innerText = proverbioDoDia;
 }
 
-// Calcule o tempo restante até as 6 da manhã
+// Calcule o tempo restante até a meia-noite no fuso horário de Brasília
 var agora = new Date();
-var seisDaManha = new Date(agora.getFullYear(), agora.getMonth(), agora.getDate(), 6, 0, 0, 0);
-if (agora > seisDaManha) {
-    seisDaManha.setDate(seisDaManha.getDate() + 1);
-}
-var tempoRestante = seisDaManha - agora;
+var meiaNoite = new Date(agora.getFullYear(), agora.getMonth(), agora.getDate() + 1, 0, 0, 0);
+var offsetBrasilia = -3; // Brasília é GMT-3
+var tempoRestante = meiaNoite - agora + offsetBrasilia * 60 * 60 * 1000;
 
 // Inicie o setInterval no horário desejado
 setTimeout(function() {
     atualizarProverbio();
     setInterval(atualizarProverbio, 24 * 60 * 60 * 1000);
 }, tempoRestante);
+
+// Limpe o cache toda vez que a página for carregada
+window.onload = function() {
+    if (window.performance) {
+        if (performance.navigation.type == 1) {
+            window.location.reload(true);
+        }
+    }
+}
+atualizarProverbio();
